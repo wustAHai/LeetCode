c52d4ef92c4a156c18d82a479600ebcb8627678a
chenz <chenzhihaiwust@outlook.com>
2021-03-30 20:56:32 +0800

    2021/3/30

--- src/mid/L74.java
+++ src/mid/L74.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * Created by chenz at 11:29 on 2021/1/5
  */
 public class L74 {
-    public boolean searchMatrix(int[][] matrix, int target) {
+/*    public boolean searchMatrix(int[][] matrix, int target) {
         int r = matrix.length;
         int c = matrix[0].length;
         if (target < matrix[0][0] || target > matrix[r - 1][c - 1]) {
@@ -30,5 +30,25 @@ public class L74 {
             return Arrays.binarySearch(matrix[i], target) >= 0;
         }
         return Arrays.binarySearch(matrix[i], target) >= 0;
+    }*/
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m =  matrix.length;
+        int n =  matrix[0].length;
+        int low=0;
+        int high=m*n-1;
+        int mid=0;
+        while (low<=high){
+            mid=(high-low)/2+low;
+            int temp=matrix[mid/n][mid%n];
+            if (temp==target){
+                return true;
+            }else if (temp>target){
+                high=mid-1;
+            }else {
+                low=mid+1;
+            }
+        }
+        return false;
     }
+
 }
